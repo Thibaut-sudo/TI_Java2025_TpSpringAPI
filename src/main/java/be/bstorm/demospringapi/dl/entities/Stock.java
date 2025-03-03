a6f71ac8907052d3c5f9ad3182739a6f43331dae
Thibaut <thibaut.lenertz@gmail.com>
2025-03-03 10:39:48 +0100

    add entities Stock

---
+++ src/main/java/be/bstorm/demospringapi/dl/entities/Stock.java
@@ -0,0 +1,20 @@
+package be.bstorm.demospringapi.dl.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Product produit;
+    private int quantiteDisponible;
+
+}
